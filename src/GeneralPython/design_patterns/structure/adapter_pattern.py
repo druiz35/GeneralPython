5938232b5ed3687f64ecee9418a202148be6f9b4
druiz35 <druiz35contact@gmail.com>
2026-07-09 17:23:10 -0500

    feat: Added other example to adapter_pattern

--- src/GeneralPython/design_patterns/structure/adapter_pattern.py
+++ src/GeneralPython/design_patterns/structure/adapter_pattern.py
@@ -10,16 +10,24 @@ class Adapter(NewService, ExistingService):
     def request(self) -> str:
         return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
 
-def client_code(target: "NewService") -> None:
-    print(target.request(), end="")
+class NewNewService:
+    def other_specific_request(self) -> str:
+        return "Coso coso coso coso"
+
+class NewAdapter(NewNewService, ExistingService):
+    def request(self) -> str:
+        return f"New Adapter: {self.other_specific_request()}"
 
 if __name__ == "__main__":
     target = NewService()
-    client_code(target)
+    print(target.request())
     print("\n")
 
     adaptee = ExistingService()
     print(f"Adaptee: {adaptee.specific_request()}", end="\n\n")
 
     adapter = Adapter()
-    client_code(adapter)
+    print(adapter.request(), end="\n\n")
+
+    new_adapter = NewAdapter()
+    print(new_adapter.request())
